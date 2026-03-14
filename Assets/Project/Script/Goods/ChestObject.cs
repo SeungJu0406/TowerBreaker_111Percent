5df6640703331c8efbeaf49f6e424def3bab5cee
Keinex <nss8164@gmail.com>
2026-03-14 19:19:45 +0900

    [Feat] 상자 오브젝트 컴포넌트 틀 구현

--- Assets/Project/Script/Goods/ChestObject.cs
+++ Assets/Project/Script/Goods/ChestObject.cs
@@ -5,14 +5,39 @@ public class ChestObject : MonoBehaviour, IHitable
     [SerializeField] private float _health = 20f;
 
 
-  
+    private bool _canHit = false;
     public float TakeDamage(Transform attacker, float damage)
     {
+        // 몹이 다 죽은 후에 때릴 수 있음
+
+
+
+
         return damage;
     }
 
     public bool TryHit(Transform attacker)
-    {
-        return true;
+    { 
+        return _canHit;
     }
+
+    private void DetectAllDiedEnemy()
+    {
+        // 몹이 다 죽은 것을 감지하고 다 죽으면
+        // _canHit을 true로 바꿔서 때릴 수 있게 해준다.
+    }
+
+    private void Die()
+    {
+        // 보상 까지는 로직
+        UserDataManager.Instance.ChestCount++;
+
+        // 보상 이펙트
+
+
+        // 비활성화
+        gameObject.SetActive(false);
+    }
+
+
 }
